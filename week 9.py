71b91f9ecf63907baca5c2bc8168ef1809b1a270
swathireddy18 <157898603+swathireddy18@users.noreply.github.com>
2025-06-03 05:33:38 +0530

    Update week 9.py

--- week 9.py
+++ week 9.py
@@ -1,35 +1,64 @@
 import nltk
 from nltk.tokenize import word_tokenize
-from nltk import pos_tag
-from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk import pos_tag
 from nltk.stem import WordNetLemmatizer
-x = open("testdoc.txt").read()
+
+
+x = open("documents.txt").read()
+
+x
+
 import nltk
 nltk.download('punkt_tab')
+
+
+#Tokenization
 tokens = word_tokenize(x)
 print(tokens)
 
- tokens = word_tokenize(x)
-  print(tokens)
+
 import nltk
- nltk.download('averaged_perceptron_tagger_eng')\
- postags = pos_tag(tokens)
- print(postags)
- stop_words = set(stopwords.words('english'))
- print(stop_words)
- li = []
- for words in tokens:
-   if words not in stop_words:
-     li.append(words)
-  print(li)
- ps = PorterStemmer()
- stemlist = []
- for words in li:
-   stemlist.append([words, ps.stem(words)])
-  print(stemlist)
- wl = WordNetLemmatizer()
-  lemilist = []
-  for words in li:
-    lemilist.append([words, wl.lemmatize(words)])
-  print(lemilist)
+nltk.download('averaged_perceptron_tagger_eng')
+
+#POS Tagging
+postags = pos_tag(tokens)
+print(postags)
+
+
+import nltk
+nltk.download('stopwords')
+
+
+#Removing stop words
+stop_words = set(stopwords.words('english'))
+print(stop_words)
+
+
+li = []
+for words in tokens:
+  if words not in stop_words:
+    li.append(words)
+print(li)
+
+
+ps = PorterStemmer()
+stemlist = []
+for words in li:
+  stemlist.append([words, ps.stem(words)])
+print(stemlist)
+
+
+
+import nltk
+nltk.download('wordnet')
+
+
+
+#Lemmatization
+wl = WordNetLemmatizer()
+lemilist = []
+for words in li:
+  lemilist.append([words, wl.lemmatize(words)])
+print(lemilist)
